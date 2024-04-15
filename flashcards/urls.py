7af65012e698cc01dc3fb1f11a2c1d9c9a0b6d9b
Lhhiep-maxcode <22028295@vnu.edu.vn>
2024-04-15 10:06:30 +0700

    checkpoint2

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
 
+app_name = "flashcards"
 urlpatterns = [
-    path('', views.topic, name="topic"),
+    path("", views.topic, name="topic"),
+    path("<str:subject>/", views.learnerList, name="learnerList")
 ]
