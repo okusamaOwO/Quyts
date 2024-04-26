417f7585fbe82fa9980be0b842f69c302ef0af73
Lhhiep-maxcode <22028295@vnu.edu.vn>
2024-04-26 22:09:51 +0700

    add a card

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from . import views
 
-app_name = "flashcards"
 urlpatterns = [
     path("", views.topic, name="topic"),
+    path("add/<str:username>", views.addCard, name="add"),
     path("<str:subject>/", views.learnerList, name="learnerList"),
     path("<str:subject>/<str:username>", views.flashsetList, name="setList"),
-    path("<str:subject>/<str:username>/<str:setname>", views.flashcardList, name="cardList")
+    path("<str:subject>/<str:username>/<str:setname>", views.flashcardList, name="cardList"),
 ]
+app_name = "flashcards"
