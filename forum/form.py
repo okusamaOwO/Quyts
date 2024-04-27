6a4d25474255f6e85122e0b996bc8650ba323aab
chuquangcan <chuquangcan231@gmail.com>
2024-04-28 00:04:39 +0700

    Vote method for post

--- forum/form.py
+++ forum/form.py
@@ -6,3 +6,10 @@ class CommentForm(forms.Form):
 class PostForm(forms.Form):
     post_title = forms.CharField(label="Title:__", widget=forms.Textarea(attrs={'rows': 1, 'cols': 40}), max_length=200)
     post_content = forms.CharField(label="Context:", widget=forms.Textarea(attrs={'rows': 5, 'cols': 40}))
+
+class VoteForm(forms.Form):
+    LIKE_DISLIKE_CHOICES = (
+        ('like', 'Like'),
+        ('dislike', 'Dislike'),
+    )
+    vote = forms.ChoiceField(choices=LIKE_DISLIKE_CHOICES, widget=forms.RadioSelect)
