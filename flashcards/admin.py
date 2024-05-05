5d9690284c479c57e12d0d4d3c6be68c28a37101
alfyrje <alfyrje@gmail.com>
2024-05-05 23:00:52 +0700

    assignment 2

--- flashcards/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Cards, Subject
-
-# Register your models here.
-admin.site.register(Cards)
-admin.site.register(Subject)
--- flashcards/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name = "flashcards"
-urlpatterns = [
-    path("", views.topic, name="topic"),
-    path("<str:subject>/", views.learnerList, name="learnerList")
-]
