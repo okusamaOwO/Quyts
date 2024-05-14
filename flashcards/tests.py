34538cb7d71d6bbdcbcb368f7396db23fd30b9cc
alfyrje <alfyrje@gmail.com>
2024-05-15 05:48:11 +0700

    added test

--- flashcards/tests.py
+++ flashcards/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.test.utils import setup_test_environment
+from django.test import Client
+from .models import Learner
 
+class FlashcardLoginTests(TestCase):
+    def setUp(self):
+        print("-------------heyHELEOEOLELE")
+        self.learner = Learner.objects.create_user(
+            username = "test",
+            password = "testpass",
+            email =  "test@example.com"
+        )
+    def test_flashcard_addcard_login(self):
+        client = Client()
+
+        logged_in = client.login(username="test", password="testpass")
+        self.assertTrue(logged_in)
+
+        response = client.get("/")
+        self.assertIsNot(response, 200)
 # Create your tests here.
+
+
